04b3f3893ef6a6ddcc21d8989e59678b595fa664
Zaid Nawaz <zaidalviza786@gmail.com>
2024-12-09 11:18:00 +0500

    Ractangle shape draw on map fixes and api data fetching

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import {MatSelectModule} from '@angular/material/select';
 import { MatDialog } from '@angular/material/dialog';
 import {MatSliderModule} from '@angular/material/slider';
 import { DatepickerDailogComponent } from '../../dailogs/datepicker-dailog/datepicker-dailog.component';
 import dayjs from 'dayjs';
+import {MatSnackBar} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-footer',
   standalone: true,
@@ -26,12 +27,21 @@ export class FooterComponent {
   @Input()longitude:any;
   @Input()latitude:any;
   @Input()zoomLevel:any;
-  startDate:Date = new Date();
-  endDate:Date = new Date();
+  startDate:any
+  endDate:any;
+  currentUtcTime:any;
+  private _snackBar = inject(MatSnackBar);
   constructor(private dialog: MatDialog){}
 
   toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
+    if(this.startDate && this.endDate){
+      this.isDropdownOpen = !this.isDropdownOpen;
+    } else {
+      this._snackBar.open('Please first select date range.', 'Ok', {
+        duration: 2000  // Snackbar will disappear after 300 milliseconds
+      });
+    }
+    
   }
   
   selectOption(option:any) {
@@ -51,6 +61,7 @@ export class FooterComponent {
         console.log('Selected date range:', result);
         this.startDate = result.startDate;
         this.endDate  = result.endDate;
+        this.currentUtcTime = result.currentUtcTime;
         // Do something with the result
         // For example: this.startDate = result.startDate; this.endDate = result.endDate;
 
@@ -67,6 +78,23 @@ export class FooterComponent {
   getFormattedDate(date: Date): string {
     return dayjs(date).format('MM.DD.YYYY');
   }
+   formatUtcTime(payload: string | Date): string {
+    // If payload is a string, convert it to Date first
+    const date = new Date(payload);
+  
+    // Check if the date is valid
+    if (isNaN(date.getTime())) {
+      throw new Error('Invalid date passed');
+    }
+  
+    // Get the UTC hours and minutes
+    const hours = date.getUTCHours().toString().padStart(2, '0');
+    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+  
+    // Return formatted time in "HH:mm UTC" format
+    return `${hours}:${minutes} UTC`;
+  }
+  
 }
 
 
